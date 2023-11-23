5460f49bfb269830b72a48d390369effab8faccf
tsioumpris <tsgiannis@gmail.com>
2023-11-23 11:57:06 +0200

    Working almost

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -7,8 +7,23 @@ from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
 from jinja2 import TemplateNotFound
+
+
+from . import blueprint
 from ..main_code import *
 from ..utilities import *
+
+#https://stackoverflow.com/questions/71285841/submit-flask-form-without-re-rendering-the-page#:~:text=If%20you%20want%20to%20submit,submitted%20in%20the%20same%20format.
+
+@blueprint.route('/route',methods=['POST'])
+def routename():
+    selected_option = request.form.get('selected_option')
+    print(selected_option          )
+    return None
+    # Process the selected option as needed
+    #return f'Selected option: {selected_option}'
+
+
 
 
 @blueprint.route('/index')
@@ -16,20 +31,21 @@ from ..utilities import *
 def index():
     data = [{'name': 'red'}, {'name': 'green'}, {'name': 'blue'}]
     datasets, language_models, deep_learning = load_config()
-    #contains_list_ = contains_list_recursive(datasets)
-    #contains_list = contains_list_recursive(language_models)
+    # contains_list_ = contains_list_recursive(datasets)
+    # contains_list = contains_list_recursive(language_models)
+
+    return render_template('home/index.html', segment='index', **locals())
 
 
-
-    return render_template('home/index.html', segment='index',**locals())
 """TODO https://jsfiddle.net/enzDx/5/"""
-#https://stackoverflow.com/questions/63628163/trying-to-get-the-bootstrap-dropdown-button-text-to-change-to-the-item-selected
+
+
+# https://stackoverflow.com/questions/63628163/trying-to-get-the-bootstrap-dropdown-button-text-to-change-to-the-item-selected
 
 
 @blueprint.route('/<template>')
 @login_required
 def route_template(template):
-
     try:
 
         if not template.endswith('.html'):
@@ -50,7 +66,6 @@ def route_template(template):
 
 # Helper - Extract current page name from request
 def get_segment(request):
-
     try:
 
         segment = request.path.split('/')[-1]
@@ -64,10 +79,4 @@ def get_segment(request):
         return None
 
 
-@blueprint.route("/route" , methods=['GET', 'POST'])
-def data_sent():
-    selected_option = request.form.get('selected_option')
-    # Process the selected option as needed
-    return f'Selected option: {selected_option}'
-    select = request.form.get('datasets')
-    return(str(select)) # just to see what select is
+
